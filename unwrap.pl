596325d77af4cf9aba43e624a40916f8b20254da
Sean Sweda <sweda@ibl.org>
2005-04-26 04:49:07 +0000

    deal with blank lines properly

--- unwrap.pl
+++ unwrap.pl
@@ -6,7 +6,7 @@ $END = 55;
 
 $line1 = <>;
 while ( $line2 = <> ) {
-    if ( (length($line2) <= 80-$END) && (length($line1) >= $END) ) {
+    if ( (length($line2) > 1 ) && (length($line2) <= 80-$END) && (length($line1) >= $END) ) {
 	if ( (length($line1) >= $END ) ) {
 	    print substr $line1,0,-1;
 	    print $line2;
