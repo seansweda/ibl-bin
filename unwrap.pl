5ba14fa38fbdaa15cf75d5d21cb2046167b873b8
Sean Sweda <sweda@ibl.org>
2005-05-03 05:40:10 +0000

    add regex to check for blank lines check wrap at 60 cols

--- unwrap.pl
+++ unwrap.pl
@@ -2,11 +2,11 @@
 #
 # $Id$
 
-$END = 55;
+$END = 60;
 
 $line1 = <>;
 while ( $line2 = <> ) {
-    if ( (length($line2) > 1 ) && (length($line2) <= 80-$END) && (length($line1) >= $END) ) {
+    if ( ( $line2 !~ /^(\s)+$/ ) && ($line2[0] ne '\n') && (length($line2) <= 80-$END) && (length($line1) >= $END) ) {
 	if ( (length($line1) >= $END ) ) {
 	    print substr $line1,0,-1;
 	    print $line2;
