2e256ca942ae339c36eb5459f60d7c9a5cfcac32
Sean Sweda <sweda@ibl.org>
2006-04-13 05:11:15 +0000

    db configuration info sourced by all scripts

---
+++ DBconfig.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+$host = 'phantasm.ibl.org';
+$dbname = ibl_stats;
+$username = 'stats';
+$password = 'st@ts=Fun';
+
+$startsdb = starts2006;
+$batdb = bat2006;
+$pitdb = pit2006;
+$teamdb = teams2006;
+$scheddb = sched2006;
+
