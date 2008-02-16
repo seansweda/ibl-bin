3bd6ea74ab52d804cbed71388b2724e4dc98980e
Sean Sweda <sweda@ibl.org>
2008-02-16 22:52:22 +0000

    update for 2008

--- DBconfig.pl
+++ DBconfig.pl
@@ -4,10 +4,10 @@ $host = 'phantasm.ibl.org';
 $dbname = ibl_stats;
 
 # tables for current season
-$startsdb = starts2007;
-$batdb = bat2007;
-$pitdb = pit2007;
-$teamdb = teams2007;
-$scheddb = sched2007;
-$extradb = extra2007;
+$startsdb = starts2008;
+$batdb = bat2008;
+$pitdb = pit2008;
+$teamdb = teams2008;
+$scheddb = sched2008;
+$extradb = extra2008;
 
