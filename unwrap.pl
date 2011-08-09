3b177a086e1ab88c4d0764acfb404cc8b00d2b62
Sean Sweda <sweda@ibl.org>
2011-08-09 20:26:57 +0000

    print space between glued lines to preserve column separation

--- unwrap.pl
+++ unwrap.pl
@@ -9,6 +9,7 @@ while ( $line2 = <> ) {
     if ( ( $line2 !~ /^(\s)+$/ ) && ($line2[0] ne '\n') && (length($line2) <= 80-$END) && (length($line1) >= $END) ) {
 	if ( (length($line1) >= $END ) ) {
 	    print substr $line1,0,-1;
+	    print " ";
 	    print $line2;
 	    $line1 = "";
 	}
