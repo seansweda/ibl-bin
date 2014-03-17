d24a3a2751b672de7c0a9af95fe12137f10fff1f
Sean Sweda <sweda@ibl.org>
2014-03-17 18:33:03 -0400

    update DBconfig for 2014

--- DBconfig.pl
+++ DBconfig.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/perl
 
-$host = 'marauders.fw.ibl.org';
+$host = 'localhost';
 $dbname = ibl_stats;
 
 # tables for current season
-$startsdb = starts2013;
-$batdb = bat2013;
-$pitdb = pit2013;
-$teamdb = teams2013;
-$scheddb = sched2013;
-$extradb = extra2013;
+$startsdb = starts2014;
+$batdb = bat2014;
+$pitdb = pit2014;
+$teamdb = teams2014;
+$scheddb = sched2014;
+$extradb = extra2014;
 
