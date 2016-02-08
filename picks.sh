1d3fd31859b6977f609a26eecaf014be4caf9c04
Sean Sweda <sweda@ibl.org>
2016-02-08 15:29:11 -0500

    better picks.sh

--- picks.sh
+++ picks.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 data=/tmp/picks.txt
-#year=${IBL_PICKSYEAR:-`date +%Y`}
 
-if [ $# -ne 0 ]; then
-    if [ x`find $data -mtime -1 2>/dev/null` != x${data} ]; then
-	picks.py >| /tmp/picks.txt
-    fi
+while getopts :t: opt; do
+    echo "getopt: \"$opt\",\"$OPTARG\""
+    case $opt in
+	t)  if [ x`find $data -mtime -1 2>/dev/null` != x${data} ]; then
+		picks.py >| $data
+	    fi
+	    awk -v team=`echo "$OPTARG" | tr a-z A-Z` '$2 ~ team' $data
+	    exit
+	    ;;
+    esac
+done
 
-    for x in $*; do
-	awk -v team=`echo $x | tr a-z A-Z` '$2 ~ team' /tmp/picks.txt
-    done
-else
-    exec picks.py
-fi
-
+exec picks.py $*
