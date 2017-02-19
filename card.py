677f46c9692d40cb7090ea11459e8317ebc93101
Sean Sweda <sweda@ibl.org>
2017-02-19 00:48:40 -0500

    card.py: add wOBA

--- card.py
+++ card.py
@@ -68,16 +68,79 @@ def p_split(p_str, lower=False):
         return None
 
 def batprint(p):
-    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s  - %4s%4s%4s %3s" % \
-        ( p[0], p[1], p[14], p[15], p[16], p[17], p[18], p[19], p[20], p[21], p[22], p[23], p[24] )
-    print "%-3s %-15s vR %4s%4s%4s%4s%4s%4s%4s  - %4s%4s%4s %3s" % \
-        ( p[0], p[1], p[26], p[27], p[28], p[29], p[30], p[31], p[32], p[33], p[34], p[35], p[36] )
+    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s  .%4i .%4s%4s%4s %3s" % \
+        ( p[0], p[1], p[14], p[15], p[16], p[17], p[18], p[19], p[20], wOBA(p, 0, 0), p[21], p[22], p[23], p[24], )
+    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s  .%4i .%4s%4s%4s %3s" % \
+        ( p[0], p[1], p[26], p[27], p[28], p[29], p[30], p[31], p[32], wOBA(p, 0, 1), p[33], p[34], p[35], p[36], )
 
 def pitprint(p):
-    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s%4s  - %4s%4s%4s" % \
-        ( p[0], p[1], p[16], p[17], p[18], p[19], p[20], p[21], p[22], p[23], p[24], p[25], p[26] )
-    print "%-3s %-15s vR %4s%4s%4s%4s%4s%4s%4s%4s  - %4s%4s%4s" % \
-        ( p[0], p[1],  p[28], p[29], p[30], p[31], p[32], p[33], p[34], p[35], p[36], p[37], p[38] )
+    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s%4s  .%4i .%4s%4s%4s" % \
+        ( p[0], p[1], p[16], p[17], p[18], p[19], p[20], p[21], p[22], p[23], wOBA(p, 1, 0), p[24], p[25], p[26] )
+    print "%-3s %-15s vL %4s%4s%4s%4s%4s%4s%4s%4s  .%4i .%4s%4s%4s" % \
+        ( p[0], p[1],  p[28], p[29], p[30], p[31], p[32], p[33], p[34], p[35], wOBA(p, 1, 1), p[36], p[37], p[38] )
+
+def wOBA(p, kind, side):
+    # wOBA consts
+    wBB = 0.70
+    w1B = 0.90
+    w2B = 1.25
+    w3B = 1.60
+    wHR = 2.00
+
+    # cardengine consts.h
+    DF = 50
+    IFR1B = 0.4306
+    IFR2B = 0.0694
+    OFR1B = 0.1121
+    OFR2B = 0.3879
+
+    def power( rating ):
+        if rating == "Ex":
+            return 0.5
+        elif rating == "Vg":
+            return 0.4
+        elif rating == "Av":
+            return 0.3
+        elif rating == "Fr":
+            return 0.2
+        else:
+            return 0.1
+
+    # pitcher
+    if kind == 1:
+        if side == 0:
+            index = 16
+        else:
+            index = 28
+
+        woba = 0
+        woba += int(p[index]) * w1B
+        woba += int(p[index + 1]) * w2B
+        woba += int(p[index + 2]) * power('Av') * wHR
+        woba += int(p[index + 3]) * wBB
+        woba += int(p[index + 4]) * wBB
+        woba += int(p[index + 5]) * IFR1B * w1B
+        woba += int(p[index + 5]) * IFR2B * w2B
+        woba += int(p[index + 6]) * OFR1B * w1B
+        woba += int(p[index + 6]) * OFR2B * w2B
+
+    # batter
+    else:
+        if side == 0:
+            index = 14
+        else:
+            index = 26
+
+        woba = 0
+        woba += int(p[index]) * w1B
+        woba += int(p[index + 1]) * w2B
+        woba += int(p[index + 2]) * w3B
+        DF_hr = DF * power( p[index + 10] )
+        woba += ( int(p[index + 3]) + DF_hr ) * wHR
+        woba += int(p[index + 4]) * wBB
+        woba += int(p[index + 5]) * wBB
+
+    return int(woba + 100.5)
 
 def main():
     global batters, pitchers, grepcmd
@@ -122,9 +185,9 @@ def main():
         datafile = datadir + '/' + pitchers
 
     if bat:
-        print "Player                   1B  2B  3B  HR  HB  BB  HG       H  OB  TB  Pw"
+        print "Player                   1B  2B  3B  HR  HB  BB  HG   wOBA     H  OB  TB  Pw"
     else:
-        print "Player                   1B  2B  DF  HB  BB IFR OFR  HG       H  OB  TB"
+        print "Player                   1B  2B  DF  HB  BB IFR OFR  HG   wOBA     H  OB  TB"
 
     if hashmode:
         cards = p_hash(datafile)
